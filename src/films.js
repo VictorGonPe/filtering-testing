0ea43b5cbf172383c095df024cf9a614f670c187
VictorGonPe <victor.gonzalez.perez.1982@gmail.com>
2025-03-20 13:56:14 +0100

    Exercises 1 and 2 completed

--- src/films.js
+++ src/films.js
@@ -1,17 +1,23 @@
+import data from "data.js";
+
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
-  let result =  ???;
+  let result =  array.map(item => item.director);
   console.log("EXERCICE 1 ->", result);
   return result;
 }
 
+getAllDirectors(data);
+
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
- 
+  let result = array.filter(item => item.director.toLowerCase().trim === director.toLowerCase().trim);
+  return result.map(item => item.title);
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {
+  //let average = array.reduce(acumulador,  => acumulador + array.score ) 
   
 }
 
