e47578846c17a4db61df4d137fb0c2f0328850cf
VictorGonPe <victor.gonzalez.perez.1982@gmail.com>
2025-03-21 14:49:09 +0100

    completed up to exercise five

--- src/films.js
+++ src/films.js
@@ -1,4 +1,4 @@
-import data from "data.js";
+import { movies } from "./data.js";
 
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
@@ -7,28 +7,42 @@ function getAllDirectors(array) {
   return result;
 }
 
-getAllDirectors(data);
-
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
-  let result = array.filter(item => item.director.toLowerCase().trim === director.toLowerCase().trim);
+  let result = array.filter(item => item.director.toLowerCase().trim() === director.toLowerCase().trim());
+  console.log("EXERCICE 2 ->", result.map(item => item.title));
   return result.map(item => item.title);
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {
-  //let average = array.reduce(acumulador,  => acumulador + array.score ) 
-  
+  const arrayDirector = array.filter(item => item.director.toLowerCase().trim() === director.toLowerCase().trim());
+  let average = arrayDirector.reduce((acumulador, film) => acumulador + film.score, 0) / arrayDirector.length;
+  console.log("EXERCICE 3 ->", average.toFixed(2))
+  return average.toFixed(2);
 }
 
 // Exercise 4:  Alphabetic order by title 
 function orderAlphabetically(array) {
-  
+  const arrayCopy = [...array];
+  let titles = arrayCopy
+    .map(film => film.title)
+    .sort();  
+  console.log("EXERCICE 4 ->", titles.slice(0,20));
+  return titles.slice(0,20);
 }
 
 // Exercise 5: Order by year, ascending
-function orderByYear() {
-
+function orderByYear(array) {
+  const arrayCopy = [...array];
+  let titles = arrayCopy.sort((a,b) => {
+    if(a.year !== b.year){
+      return a.year - b.year;
+    }else{
+      return a.title.localeCompare(b.title);
+    }
+  }); 
+    console.log("EXERCICE 5 ->", titles);
 }
 
 // Exercise 6: Calculate the average of the movies in a category
