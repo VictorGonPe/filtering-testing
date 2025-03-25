5e94b54d693c82d5638c2339a3f3e54cb8a6fb69
VictorGonPe <victor.gonzalez.perez.1982@gmail.com>
2025-03-25 18:37:55 +0100

    Delete comments

--- src/films.js
+++ src/films.js
@@ -3,7 +3,6 @@ const movies = require('./data');
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
   let result = array.map(item => item.director);
-  //console.log("EXERCICE 1 ->", result);
   return result;
 }
 
@@ -11,7 +10,6 @@ function getAllDirectors(array) {
 function getMoviesFromDirector(array, director) {
   if (!Array.isArray(array) || typeof director !== 'string') return [];
   let result = array.filter(item => item.director.toLowerCase().trim() === director.toLowerCase().trim());
-  //console.log("EXERCICE 2 ->", result.map(item => item.title));
   return result;
 }
 
@@ -19,7 +17,6 @@ function getMoviesFromDirector(array, director) {
 function moviesAverageOfDirector(array, director) {
   const arrayDirector = array.filter(item => item.director.toLowerCase().trim() === director.toLowerCase().trim());
   const average = arrayDirector.reduce((acumulador, film) => acumulador + film.score, 0) / arrayDirector.length;
-  //console.log("EXERCICE 3 ->", average.toFixed(2))
   return Number(average.toFixed(2));
 }
 
@@ -29,11 +26,9 @@ function orderAlphabetically(array) {
   let titles = arrayCopy
     .map(film => film.title)
     .sort();
-  //console.log("EXERCICE 4 ->", titles.slice(0,20));
   return titles.slice(0, 20);
 }
 
-// Exercise 5: Order by year, ascending
 function orderByYear(array) {
   const arrayCopy = [...array];
   let titles = arrayCopy.sort((a, b) => {
@@ -43,10 +38,10 @@ function orderByYear(array) {
       return a.title.localeCompare(b.title);
     }
   });
-  // console.log("EXERCICE 5 ->", titles);
+ 
   return titles;
 }
-// Exercise 6: Calculate the average of the movies in a category
+
 function moviesAverageByCategory(array, genre) {
   const formatGenre = genre.toLowerCase().trim();
 
@@ -62,10 +57,6 @@ function moviesAverageByCategory(array, genre) {
   return average.toFixed(2);
 }
 
-//console.log("EXERCICE 6 -> ",moviesAverageByCategory(movies,'crime'));
-
-
-// Exercise 7: Modify the duration of movies to minutes
 function hoursToMinutes(array) {
   const regex1 = /(\d+)h/;
   const regex2 = /(\d+)min/;
@@ -81,9 +72,7 @@ function hoursToMinutes(array) {
   })
   return arrayCopy;
 }
-//console.log("EXERCICE 7 -> ", hoursToMinutes(movies));
 
-// Exercise 8: Get the best film of a year
 function bestFilmOfYear(array, year) {
   const bestFilm = array.filter(film => film.year === year);
 
@@ -94,11 +83,8 @@ function bestFilmOfYear(array, year) {
   })
   return [theBest];
 }
-//console.log("EXERCICE 8 -> ",bestFilmOfYear(movies,2001));
 
 
-// The following is required to make unit tests work.
-/* Environment setup. Do not modify the below code. */
 if (typeof module !== 'undefined') {
   module.exports = {
     getAllDirectors,
